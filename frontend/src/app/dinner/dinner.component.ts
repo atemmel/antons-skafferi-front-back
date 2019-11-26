2b18c944ee2a4e1456685889206495c09cada2bf
typelias <98ille@gmail.com>
2019-11-26 11:45:52 +0100

    Fixed styling and made the menu more dynamic

--- frontend/src/app/dinner/dinner.component.ts
+++ frontend/src/app/dinner/dinner.component.ts
@@ -21,20 +21,20 @@ export class DinnerComponent implements OnInit {
   getMenu() {
     let jsonObject: JSON;
     const jsonString: any = {
-      "Kockens Val": [{"title": "Vildssvin", "description": "Vildssvinskottlett med potatismos och lingonsylt"},
+      "Kockens Val": { "array": [{"title": "Vildssvin", "description": "Vildssvinskottlett med potatismos och lingonsylt"},
         {"title": "Vild lax", "description": "Lax fångad vilt med färskpotatis som tillbehör"},
-        {"title": "Renskav", "description": "Renskav med potatismos"}],
-      "Förrätter": [{"title": "Skagentoast", "description": "Toast med skagenröra och en citronskiva"},
+        {"title": "Renskav", "description": "Renskav med potatismos"}], "picture" : ""},
+      "Förrätter": { "array": [{"title": "Skagentoast", "description": "Toast med skagenröra och en citronskiva"},
         {"title": "Vitlöksbröd", "description": "Baguett med vitlökssmör"},
-        {"title": "Mozarellasticks", "description": "Friterad mozarella"}],
-      "Vilt": [{"title": "Renskav", "description": "Renskav med potatismos"},
+        {"title": "Mozarellasticks", "description": "Friterad mozarella"}], "picture": "https://receptfavoriter.se/sites/default/files/vitloksbrod_65_980.jpg" },
+      "Vilt": {"array": [{"title": "Renskav", "description": "Renskav med potatismos"},
         {"title": "Älgfilè", "description": "Älgfilè med potatisgrattäng"},
-        {"title": "Bäverstek", "description": "Bäverstek med potatis"}],
-      "À La Carte": [{"title": "Lax", "description": "Lax med potatismos"},
+        {"title": "Bäverstek", "description": "Bäverstek med potatis"}], "picture": "https://images.ctfassets.net/wcifomac350q/74mxcPcxFKpghUOMKAGsyA/0b114891ec21bff5df58912bf7b018e5/img_viltbord_matbild.jpg?fm=webp&q=70" },
+      "À La Carte": {"array": [{"title": "Lax", "description": "Lax med potatismos"},
         {"title": "Oxfilè", "description": "Oxfilè med potatisgratäng"},
-        {"title": "Kötbullar", "description": "Köttbullar med potatismos med lingonsylt"}],
-      "Dryck": [{"title": "Cola", "description": "Burk(33cl)"},
-        {"title": "ÖL", "description": "Stor ÖL"}]
+        {"title": "Kötbullar", "description": "Köttbullar med potatismos med lingonsylt"}], "picture": "https://assets.icanet.se/q_auto,f_auto/imagevaultfiles/id_152536/cf_259/helstekt-oxfile-bg-1920x540.jpg?" },
+      "Dryck": {"array": [{"title": "Cola", "description": "Burk(33cl)"},
+        {"title": "ÖL", "description": "Stor ÖL"}], "picture": "https://www.kingsizemag.se/wp-content/uploads/2019/05/beer-ls-580x345.jpg" }
     };
     jsonObject = jsonString as JSON;
 
@@ -42,7 +42,8 @@ export class DinnerComponent implements OnInit {
 
     for (let key in jsonObject) {
       let temp: MenuItem = new MenuItem(key);
-      const temparray: Dish[] = jsonObject[key];
+      temp.pictureUrl = jsonObject[key].picture;
+      const temparray: Dish[] = jsonObject[key].array;
 
       temp.replaceDishes(temparray);
       this.menu.push(temp);
@@ -68,6 +69,7 @@ class Dish {
 
 class MenuItem {
   title: string;
+  pictureUrl: string;
   public dishes: Array<Dish> = [];
 
   constructor(title: string) {
