d7649dcefdca6e47e0cf283eea18804ea54522a9
Typelias <98ille@gmail.com>
2019-11-27 13:57:12 +0100

    Cleaned html and css code for dinner section and made navbar sticky

--- frontend/src/app/dinner/dinner.component.ts
+++ frontend/src/app/dinner/dinner.component.ts
@@ -26,21 +26,26 @@ export class DinnerComponent implements OnInit {
         {"title": "Renskav", "description": "Renskav med potatismos"}], "picture" : ""},
       "Förrätter": { "array": [{"title": "Skagentoast", "description": "Toast med skagenröra och en citronskiva"},
         {"title": "Vitlöksbröd", "description": "Baguett med vitlökssmör"},
-        {"title": "Mozarellasticks", "description": "Friterad mozarella"}], "picture": "https://receptfavoriter.se/sites/default/files/vitloksbrod_65_980.jpg" },
+        {"title": "Mozarellasticks", "description": "Friterad mozarella"}],
+        "picture": "https://receptfavoriter.se/sites/default/files/vitloksbrod_65_980.jpg" },
       "Vilt": {"array": [{"title": "Renskav", "description": "Renskav med potatismos"},
         {"title": "Älgfilè", "description": "Älgfilè med potatisgrattäng"},
-        {"title": "Bäverstek", "description": "Bäverstek med potatis"}], "picture": "https://images.ctfassets.net/wcifomac350q/74mxcPcxFKpghUOMKAGsyA/0b114891ec21bff5df58912bf7b018e5/img_viltbord_matbild.jpg?fm=webp&q=70" },
+        {"title": "Bäverstek", "description": "Bäverstek med potatis"}],
+        "picture": "https://images.ctfassets.net/wcifomac350q/74mxcPcxFKpghUOMKAGsyA/0b114891ec21bff5df58912bf7b018e5/img_viltbord_matbild.jpg?fm=webp&q=70" },
       "À La Carte": {"array": [{"title": "Lax", "description": "Lax med potatismos"},
         {"title": "Oxfilè", "description": "Oxfilè med potatisgratäng"},
-        {"title": "Kötbullar", "description": "Köttbullar med potatismos med lingonsylt"}], "picture": "https://assets.icanet.se/q_auto,f_auto/imagevaultfiles/id_152536/cf_259/helstekt-oxfile-bg-1920x540.jpg?" },
+        {"title": "Kötbullar", "description": "Köttbullar med potatismos med lingonsylt"}],
+        "picture": "https://assets.icanet.se/q_auto,f_auto/imagevaultfiles/id_152536/cf_259/helstekt-oxfile-bg-1920x540.jpg?" },
       "Dryck": {"array": [{"title": "Cola", "description": "Burk(33cl)"},
-        {"title": "ÖL", "description": "Stor ÖL"}], "picture": "https://www.kingsizemag.se/wp-content/uploads/2019/05/beer-ls-580x345.jpg" }
+        {"title": "ÖL", "description": "Stor ÖL"},
+        {"title": "Vinflarra", "description": "Speedrun"}],
+        "picture": "https://www.kingsizemag.se/wp-content/uploads/2019/05/beer-ls-580x345.jpg" }
     };
     jsonObject = jsonString as JSON;
 
     console.log(jsonObject);
 
-    for (let key in jsonObject) {
+    for (const key of Object.keys(jsonObject)) {
       let temp: MenuItem = new MenuItem(key);
       temp.pictureUrl = jsonObject[key].picture;
       const temparray: Dish[] = jsonObject[key].array;
