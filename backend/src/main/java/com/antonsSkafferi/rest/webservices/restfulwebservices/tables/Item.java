1598be7a2fc1705005eb174f062959941cca7dfc
iSellgren <frse1700@student.miun.se>
2019-12-01 15:46:37 +0100

    Addad auto increment to item

--- backend/src/main/java/com/antonsSkafferi/rest/webservices/restfulwebservices/tables/Item.java
+++ backend/src/main/java/com/antonsSkafferi/rest/webservices/restfulwebservices/tables/Item.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -18,6 +20,7 @@ public class Item implements Serializable {
 
     //@Id Specifies the primary key of an entity
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "itemid", updatable = false, nullable = false)
     private int itemid;
     
