a42b7082f7b964d1d68c093d7b7c1abf882294af
Typelias <98ille@gmail.com>
2019-12-04 12:02:05 +0100

    Added items in sql file

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -29,3 +29,21 @@ VALUES (3,'Fiskgryta',115,'sej, musslor, potatis, fänkål, dill, citron, aioli'
 
 insert into ITEM (itemid, title, price, description, type, itemcategoryid)
 VALUES (4,'Tomatsoppa',115,'Ägg 63, chili, krutonger, basilika','vegetarisk',9);
+
+insert into ITEM (itemid, title, price, description, type, itemcategoryid)
+VALUES (5,'Cola',115,'33CL','dryck',7);
+
+insert into ITEM (itemid, title, price, description, type, itemcategoryid)
+VALUES (6,'Skakentoast',115,'Toast med skagenröra','macka',2);
+
+insert into ITEM (itemid, title, price, description, type, itemcategoryid)
+VALUES (7,'Renskav',115,'Renskav med potatis','Vilt',4);
+
+insert into ITEM (itemid, title, price, description, type, itemcategoryid)
+VALUES (8,'Glass',115,'3 skopor vaniljglass med colasås','glass',3);
+
+insert into ITEM (itemid, title, price, description, type, itemcategoryid)
+VALUES (9,'Vildssvin',115,'Vildssvinskotlett med potatisgratäng','vilt',5);
+
+insert into ITEM (itemid, title, price, description, type, itemcategoryid)
+VALUES (10,'Spagetti med kötfärsås',115,'Serveras med parmesanost','pasta',6);
