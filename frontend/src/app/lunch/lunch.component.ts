5c0228154ea02e2d53a3cabb3c1a3a35066918f8
Gustav_Nilsson <nilsson.gustav97@gmail.com>
2019-12-04 16:16:18 +0100

    Added logic to get data from backend and display it in the lunchmenus

--- frontend/src/app/lunch/lunch.component.ts
+++ frontend/src/app/lunch/lunch.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { GetImageService } from '../services/data/get-image.service';
-import { DomSanitizer } from '@angular/platform-browser';
 import { getLocaleDayNames } from '@angular/common';
+import { GetLunchesService } from '../services/data/get-lunches.service';
 
 @Component({
   selector: 'app-lunch',
@@ -10,111 +9,36 @@ import { getLocaleDayNames } from '@angular/common';
 })
 export class LunchComponent implements OnInit {
 
-  lunchMeals: any;
+  lunchMenuResponse: any;
+  lunchMenuLoaded: Promise<boolean>;
+  lunchMenu: Array<LunchMeal> = [];
   dailyLunch: any;
   days: any[] = [[], [], [], [], []];
-  image: any;
-  imageName: string = "restaurant_1920x1280.jpg";
-  folderName: string = "website";
 
-  constructor(private service: GetImageService, private sanitizer: DomSanitizer) { }
+  constructor(private lunchGetter: GetLunchesService) { }
 
   ngOnInit() {
     this.getLunchMenu();
-    this.getDailyLunch();
+    
   }
 
   getDailyLunch() {
     const date: number = (new Date().getDay());
-    this.dailyLunch = this.lunchMeals.filter(meal => meal.day == date)
+    this.dailyLunch = this.lunchMenuResponse.filter(meal => meal.days == date)
     //console.log(this.dailyLunch);
   }
 
-  getLunchMenu() {
+  async getLunchMenu() {
 
+    const lunchMenuResponse = await this.lunchGetter.getLunches().toPromise();
+    this.lunchMenuResponse = lunchMenuResponse;
+    this.lunchMenuLoaded = Promise.resolve(true);
+    console.log(lunchMenuResponse);
     // 0 is sunday, 1 is monday, 2 is tuesday, 3 is wednesday, 4 is thursday, 5 is friday, 6 is saturday
-    this.lunchMeals = [
-      {
-        "mealName": "Grön Sparrissoppa",
-        "day": 5,
-        "type": "Vegetariskt"
-      },
-      {
-        "mealName": "Panerad rödspetta med skagenröra",
-        "day": 5,
-        "type": "Fisk"
-      },
-      {
-        "mealName": "Kycklingfilé med champinjonsås och stekt potatis",
-        "day": 5,
-        "type": "För Köttälskare"
-      },
-      {
-        "mealName": "Lax & spenatsoppa",
-        "day": 1,
-        "type": "Fisk"
-      },
-      {
-        "mealName": "Spagetti bolognaise",
-        "day": 2,
-        "type": "Dagens Pasta"
-      },
-      {
-        "mealName": "Stekt fläsk med löksås",
-        "day": 3,
-        "type": "För köttälskare"
-      },
-      {
-        "mealName": "Clam Showder på 'Antons vis' med aioli",
-        "day": 4,
-        "type": "Fisk"
-
-      },
-      {
-        "mealName": "Örtbakad torskfilé med gratinerad potatismos",
-        "day": 2,
-        "type": "Fisk"
-      },
-      {
-        "mealName": "Pastagratäng med rökt kassler purjolök och cocktailtomater",
-        "day": 3,
-        "type": "Pastarätt"
-      },
-      {
-        "mealName": "Ärtsoppa med pannkakor, grädde & sylt",
-        "day": 5,
-        "type": "Klassiker"
-      },
-      {
-        "mealName": "Laxpudding med skirat smör och lingonsylt",
-        "day": 4,
-        "type": "Fisk"
-      },
-      {
-        "mealName": "Kalops på högrev kokt potatis och rödbetor",
-        "day": 4,
-        "type": "Kötträtt"
-      },
-      {
-        "mealName": "Köttfärsås och spaghetti",
-        "day": 2,
-        "type": "Kött"
-      },
-      {
-        "mealName": "Fish and chips med remouladsås",
-        "day": 3,
-        "type": "Fiskrätt"
-      },
-      {
-        "mealName": "Stek",
-        "day": 1,
-        "type": "Kötträtt"
-      }
-    ];
 
     /**Sorts every individual day so that they're all ordered after type alphabetically*/
     for(var i = 0; i < this.days.length; i++) {
-      this.days[i] = this.lunchMeals.filter(meal => meal.day == i + 1);
+      this.days[i] = this.lunchMenuResponse.filter(meal => meal.days == i + 1);
       this.days[i].sort((lhs, rhs) => {
         var lhsType = lhs.type.toUpperCase();
         var rhsType = rhs.type.toUpperCase();
@@ -128,5 +52,20 @@ export class LunchComponent implements OnInit {
         return 0;
       });
     }
+    this.getDailyLunch();
+  }
+  
+}
+
+class LunchMeal {
+  mealName: string;
+  day: number;
+  mealType: string;
+
+
+  constructor(mealName: string, day: number, mealType: string) {
+    this.mealName = mealName;
+    this.day = day;
+    this.mealType = mealType; 
   }
 }
