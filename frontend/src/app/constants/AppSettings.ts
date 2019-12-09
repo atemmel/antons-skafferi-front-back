736918bf15132d2498cdd26424e13aa030b51c73
Typelias <98ille@gmail.com>
2019-12-09 13:32:10 +0100

    Change server adress to actual server

--- frontend/src/app/constants/AppSettings.ts
+++ frontend/src/app/constants/AppSettings.ts
@@ -1,8 +1,8 @@
 
-export class AppSettings{
+export class AppSettings {
 
     constructor() { }
 
-    public static BACKEND_URL: string = "http://localhost:8080";
+    public static BACKEND_URL: string = "http://antons-skafferi.tk:8080";
     public static IMAGES_PATH: string = "/images?";
 }
