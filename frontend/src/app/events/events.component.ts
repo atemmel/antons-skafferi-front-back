4fe2013d54978d4fb6479ae73043bea8179ab071
Gustav_Nilsson <nilsson.gustav97@gmail.com>
2019-12-10 18:00:50 +0100

    Added view for picture gallery to specific events

--- frontend/src/app/events/events.component.ts
+++ frontend/src/app/events/events.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 
 export class EventsComponent implements OnInit {
 
-  events: Event[];
+  events: Array<Event> = [];
   newestEvents: Event[];
+  imagePath: string = "../assets/images/lax5.jpg";
 
   constructor() { }
 
@@ -21,8 +22,15 @@ export class EventsComponent implements OnInit {
     const jsonString = '[ {"title": "Carola", "date": "11/12-19"}, {"title": "Lasses orkester", "date": "12/12-19"}, ' +
       '{"title": "Tummels trumma", "date": "15/12-19"}, ' +
       '{"title": "Antons gitar trio", "date": "19/12-19"}, ' +
-      '{"title": "Gurras ståbasstämma", "date": "24/12-19"}]'
-    this.events = JSON.parse(jsonString) ;
+      '{"title": "Gurras ståbasstämma", "date": "24/12-19"}]';
+    const jsonObj = JSON.parse(jsonString);
+
+    jsonObj.forEach(val => {
+      const temp: Event = new Event(val.title, val.date);
+      this.events.push(temp);
+    });
+    
+    // this.events = JSON.parse(jsonString) ;
 
     if ( this.events != null) {
     const eventLength: number = this.events.length;
@@ -37,18 +45,33 @@ export class EventsComponent implements OnInit {
     }
     }
 
-    //console.log(this.newestEvents);
-    //console.log(this.events);
+    console.log(this.newestEvents);
+    console.log(this.events);
 
   }
 
+  getEventPictures(date: string) {
+    const event: Event = this.events.find(x => x.date === date);
+    event.pictures = Array<string>();
+    if(event) {
+      for (let i = 0; i<5; i++) {
+        event.pictures.push(this.imagePath);
+      }
+    }
+  }
+  
+
 }
+
+
 
 class Event {
   title: string;
   date: string;
+  pictures: Array<string> = [];
   constructor(title: string, date: string){
     this.title = title;
     this.date = date;
   }
+
 }
