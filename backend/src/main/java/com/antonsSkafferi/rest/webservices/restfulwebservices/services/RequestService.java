da5e66aacfe8b54620bceaf7e62899c2ab7d255a
Ninhow <allamoolsson09@gmail.com>
2019-12-11 19:53:28 +0100

    Flyttat request till egen klass

--- backend/src/main/java/com/antonsSkafferi/rest/webservices/restfulwebservices/services/RequestService.java
+++ backend/src/main/java/com/antonsSkafferi/rest/webservices/restfulwebservices/services/RequestService.java
@@ -5,6 +5,7 @@
  */
 package com.antonsSkafferi.rest.webservices.restfulwebservices.services;
 
+import com.antonsSkafferi.rest.webservices.restfulwebservices.classes.Request;
 import java.util.ArrayList;
 
 /**
@@ -12,51 +13,7 @@ import java.util.ArrayList;
  * @author ninhow
  */
 public class RequestService {
-    
-    public class Request {
-        public int employeeId1;
-        public int employeeId2;
-
-        public int scheduleId1;
-        public int scheduleId2;
-
-        public Request(int employeeId1, int employeeId2, int scheduleId1, int scheduleId2) {
-            this.employeeId1 = employeeId1;
-            this.employeeId2 = employeeId2;
-            this.scheduleId1 = scheduleId1;
-            this.scheduleId2 = scheduleId2;
-        }
-
-        
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final Request other = (Request) obj;
-            if (this.employeeId1 != other.employeeId1) {
-                return false;
-            }
-            if (this.employeeId2 != other.employeeId2) {
-                return false;
-            }
-            if (this.scheduleId1 != other.scheduleId1) {
-                return false;
-            }
-            if (this.scheduleId2 != other.scheduleId2) {
-                return false;
-            }
-            return true;
-        }
-        
-    }
-    
+       
     ArrayList<Request> requests = new ArrayList<>();
     
     private static RequestService requestService;
