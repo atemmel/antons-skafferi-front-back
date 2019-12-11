dfbec468972d41b1b0e775b12a161873615f6a8c
Ninhow <allamoolsson09@gmail.com>
2019-12-11 15:57:42 +0100

    Had a wrong merge file, already fixed

--- backend/src/main/java/com/antonsSkafferi/rest/webservices/restfulwebservices/services/EventService.java
+++ backend/src/main/java/com/antonsSkafferi/rest/webservices/restfulwebservices/services/EventService.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -60,57 +60,3 @@ public class EventService {
     
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.antonsSkafferi.rest.webservices.restfulwebservices.services;
-
-import com.antonsSkafferi.rest.webservices.restfulwebservices.dataAccessObject.EventRepository;
-import com.antonsSkafferi.rest.webservices.restfulwebservices.tables.Event;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
-
-/**
- *
- * @author fredriksellgren
- */
-@Service
-public class EventService {
-    
-    @Autowired
-    EventRepository eventRepository;
-    
-    private static EventService EventService;
-    
-    public static EventService getInstance(){
-        return EventService;
-    }
-
-    public void saveOrUpdateEvent(Event event) {
-       eventRepository.save(event);
-    }
-    public void createEvent(String date) throws FileNotFoundException, IOException{
-        System.out.println(date);
-        new File("../backend/src/main/resources/images/"+date).mkdirs();
-        File file = ResourceUtils.getFile("../backend/src/main/resources/images/"+date);
-        
-        if(file.exists()){
-           File temp = new File("../backend/src/main/resources/images/"+date+"/testar.png");
-           //Create the file
-           if (temp.createNewFile()){
-             System.out.println("File is created!");
-           }else{
-             System.out.println("File already exists.");
-           }
-        }
-        
-    }
-}
->>>>>>> d37f95150ed30e95d4af7443498bf44b98bbb191
